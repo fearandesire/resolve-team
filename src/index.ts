18afed9725f9f4f9088171dd9942f201978b832b
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-19 02:12:50 -0500

    ✨ (index.ts): Replace resolveTeam.js with comprehensive searching and validation logic using fuse.js, which offers flexible and efficient fuzzy search capability, and introduce robust input validation functions ♻️ (index.ts): Refactor resolveTeam to return exact name of the team as default but allows full team object if 'full' option is chosen

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,62 @@
-import resolveTeam from "./resolveTeam.js";
+import Fuse from 'fuse.js';
+import teamList from './teamlist.js';
 
-export default resolveTeam;
+type Team = {
+    name: string;
+    colors: string[];
+    nicknames: string[];
+};
+
+type TeamList = {
+    nba: Team[];
+    nfl: Team[];
+};
+
+type Options = {
+    threshold?: number;
+    full?: boolean;
+};
+
+// Updated SearchResult type
+type SearchResult = Team | string | null;
+
+function validateInputs(sport: string, query: string): string | null {
+    if (!(sport in teamList)) {
+        return 'Invalid sport category. Please choose from ' + Object.keys(teamList).join(', ') + '.';
+    }
+    if (typeof query !== 'string' || query.trim().length === 0) {
+        return 'Invalid query. Please provide a non-empty string.';
+    }
+    return null;
+}
+
+function initializeFuse(sport: string, options: Options){
+    const searchOptions = {
+        isCaseSensitive: false,
+        shouldSort: true,
+        threshold: options.threshold,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: ['name', 'nicknames', 'abbrev'],
+    };
+
+    return new Fuse(teamList[sport as keyof TeamList], searchOptions);
+}
+
+export default function resolveTeam(sport: string, query: string, options: Options = { threshold: 0.4, full: false }): SearchResult {
+    const errorMessage = validateInputs(sport, query);
+    if (errorMessage) {
+     return null
+    }
+
+    const fuse = initializeFuse(sport, options);
+    const result = fuse.search(query);
+
+    if (result.length > 0) {
+        return options.full ? result[0].item : result[0].item.name;
+    } else {
+        return null
+    }
+}
