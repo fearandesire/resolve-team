adb88f3084916e553822ef7c64b2caea8f5e1908
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-20 11:38:49 -0500

    refactor: :recycle: Simplify export
    Simplify `index.ts` to only export the function we are looking for
    Move code to `resolver.ts`

---
+++ src/resolver.ts
@@ -0,0 +1,52 @@
+import Fuse from 'fuse.js';
+import teamList from './teamlist.js';
+import { TeamList, Team, Options, defaultOptions } from './interfaces';
+
+
+
+function validateInputs(team: string, sport: string = 'all'): string | null {
+    if (!(sport in teamList) && sport !== 'all') {
+        return 'Invalid sport category. Please choose from ' + Object.keys(teamList).join(', ') + '.';
+    }
+    if (typeof team !== 'string' || team.trim().length === 0) {
+        return 'Invalid query. Please provide a non-empty string.';
+    }
+    return null;
+}
+
+
+function initializeFuse(options: Options): Fuse<Team> {
+    const { sport } = options
+
+    const allSportsTeams = Object.values(teamList).flat()
+    const combinedTeams =  sport === 'all'
+        ? allSportsTeams
+        : teamList[sport as keyof TeamList];
+    const searchOptions = {
+        isCaseSensitive: false,
+        shouldSort: true,
+        minMatchCharLength: 1,
+        keys: ['name', 'nicknames', 'abbrev'],
+    };
+    return new Fuse(combinedTeams, searchOptions);
+}
+
+export default function resolveTeam(query: string, options: Options = defaultOptions): string | object | null {
+    const finalOptions = { ...defaultOptions, ...options };
+    const errorMessage = validateInputs(query, finalOptions.sport);
+    if (errorMessage) {
+        return null;
+    }
+
+    if (!options?.sport) {
+        options.sport = 'all'
+    }
+    const fuse = initializeFuse(options);
+    const result = fuse.search(query);
+
+    if (result.length > 0) {
+        return options.full ? result[0].item : result[0].item.name;
+    } else {
+        return null
+    }
+}
