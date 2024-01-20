4f392a6713bbe23c1b4c9a93fda0fe036dee2ed8
fearandesire <63979371+fearandesire@users.noreply.github.com>
2024-01-20 00:51:26 -0500

    ✨ (resolveTeam.spec.ts) Expand mocked nba team list to include more teams for comprehensive testing. ♻️ (resolveTeam.spec.ts) Refactor test cases to reflect new API changes on resolveTeam function. Now resolveTeam can search for a team across all leagues without specifying the league.

--- src/test/resolveTeam.spec.ts
+++ src/test/resolveTeam.spec.ts
@@ -1,27 +1,35 @@
-import resolveTeam from '../index.js'; // Adjust the import path as needed
+import resolveTeam from '../index'; // Adjust the import path as needed
 
 // Mock teamList for testing
 // Assuming teamList has a similar structure to this
 jest.mock('../../src/teamlist', () => ({
-    nba: [{ name: 'Boston Celtics', colors: ['Green', 'White'], nicknames: ['Celtics', 'BOS'] }],
+    nba: [{ name: 'Boston Celtics', colors: ['Green', 'White'], nicknames: ['Celtics', 'BOS'] },
+        { name: 'Brooklyn Nets', colors: ['Black', 'White'], nicknames: ['Nets', 'BKN'] },
+        {
+            name: 'Charlotte Hornets',
+            colors: ['#1D1160', '#00788C', '#000000'],
+            nicknames: ['hornets', 'charlotte', 'cha'],
+            abbrev: ['CHA']
+        }],
     nfl: [{ name: 'Chicago Bears', colors: ['Navy', 'Orange'], nicknames: ['Bears', 'CHI'] }]
 }));
 
 describe('resolveTeam function', () => {
     test('resolves NBA team name using abbreviation', () => {
-        expect(resolveTeam('nba', 'BOS')).toBe('Boston Celtics');
+        expect(resolveTeam('BOS')).toBe('Boston Celtics');
     });
 
     test('resolves NFL team name using partial name', () => {
-        expect(resolveTeam('nfl', 'Chicago')).toBe('Chicago Bears');
+        expect(resolveTeam('bears')).toBe('Chicago Bears');
     });
 
     test('returns full NBA team object when full option is true', () => {
-        const expected = {
-            name: 'Boston Celtics',
-            colors: ['Green', 'White'],
-            nicknames: ['Celtics', 'BOS']
-        };
-        expect(resolveTeam('nba', 'BOS', { full: true })).toEqual(expected);
+        const expected =     {
+            name: 'Charlotte Hornets',
+            colors: ['#1D1160', '#00788C', '#000000'],
+            nicknames: ['hornets', 'charlotte', 'cha'],
+            abbrev: ['CHA']
+        }
+        expect(resolveTeam('horntd', { full: true, })).toEqual(expected);
     });
 });
